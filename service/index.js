296971c6330292232a3e3fb09d8884abf44d7d40
MattSteveMitch <mattstevemitch@gmail.com>
2026-04-09 02:41:23 -0600

    Account creation is now remembered on server side

--- service/index.js
+++ service/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
 const app = express();
 
-//app.use(cookieParser());
-//app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static('public'));
 
 const router = express.Router();
 app.use("/api", router);
@@ -11,20 +13,19 @@ app.use("/api", router);
 const authTokens = Object();
 
 const emails = new Set();
-const usernames = new Set(["joe"]);
-let n = 0;
+const usernames = new Set();
+const accounts = new Object();
+
 console.log("starting up server");
 
 router.get("/validEmail/:email", async (request, result) => {
     let userEmail = request.params.email;
     console.log(userEmail);
-  //  console.log(emails);
     if (emails.has(userEmail)) {
         result.status(409);
         result.send();
     }
     else {
-      //  console.log("got request " + n);
         result.status(200);
         result.send();
     }
@@ -32,147 +33,39 @@ router.get("/validEmail/:email", async (request, result) => {
 
 router.get("/validUsername/:username", async (request, result) => {
     let username = request.params.username;
-    console.log(username);
-  //  console.log(emails);
     if (usernames.has(username)) {
         result.status(409);
         result.send();
     }
     else {
-      //  console.log("got request " + n);
         result.status(200);
         result.send();
     }
 });
 
+router.post("/account", async (request, result) => {
+    if (emails.has(request.body.email)) {
+        result.status(409);
+        result.send({msg: "Email already registered"});
+    }
+    else if (usernames.has(request.body.username)) {
+        result.status(409);
+        result.send({msg: "Username already taken"});
+    }
+    else {
+        emails.add(request.body.email);
+        usernames.add(request.body.username);
+        console.log("checkpoint");
+        bcrypt.hash(request.body.password, 12).then(
+            (hashedPass) => {
+                console.log("checkpoint 2");
+                accounts[request.body.username] = [hashedPass, request.body.email];
+                result.status(201);
+                result.send();
+            }
+        );
+    }
+});
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 app.listen(port, () => {console.log("listening");});
-/*
-function notEmpty(fields, emptyMsgs, index, errorMsgRef) {
-    if (!fields[index]) {
-        errorMsgRef.current.innerHTML = emptyMsgs[index];
-        errorMsgRef.current.className = "errorMsg bad";
-        return false;
-    }
-
-    return true;
-}
-
-export function checkUniqueEmail(fields, emptyMsgs, errorMsgRef) {
-    if (!notEmpty(fields, emptyMsgs, 0, errorMsgRef)) {
-    }
-    else if (!localStorage.getItem(fields[0] + "_email_taken")) {
-        errorMsgRef.current.innerHTML = "";
-        errorMsgRef.current.className = "errorMsg good";
-        return true;
-    }
-    else {
-        errorMsgRef.current.innerHTML = "This email already has an account";
-        errorMsgRef.current.className = "errorMsg bad";
-    }
-    return false;
-}
-
-export function checkUniqueUsername(fields, emptyMsgs, errorMsgRef) {
-//    The reason why I do all the previous check along with the current check, even if 
-  //  the previous checks have already been done, is because I want the error messages 
-    //from the first fields that you fill out to have priority. I'm a little nitpicky 
-    //like that.
-    if (!checkUniqueEmail(fields, emptyMsgs, errorMsgRef) ||
-        !notEmpty(fields, emptyMsgs, 1, errorMsgRef)) {
-    }
-    else if (!localStorage.getItem(fields[1] + "_password")) {
-        errorMsgRef.current.innerHTML = "Username available";
-        errorMsgRef.current.className = "errorMsg good";
-        return true;
-    }
-    else {
-        errorMsgRef.current.innerHTML = "Username already taken";
-        errorMsgRef.current.className = "errorMsg bad";
-        return false;
-    }
-}
-
-export function checkRegPassword(fields, emptyMsgs, errorMsgRef) {
-    // See previous comment
-    if (!checkUniqueUsername(fields, emptyMsgs, errorMsgRef) || 
-        !notEmpty(fields, emptyMsgs, 2, errorMsgRef)) {
-            return false;
-    }
-    clearError(errorMsgRef);
-
-    return true;
-}
-
-export function checkPasswordsMatch(fields, emptyMsgs, errorMsgRef) {
-    // See previous comment
-    if (!checkRegPassword(fields, emptyMsgs, errorMsgRef) || 
-        !notEmpty(fields, emptyMsgs, 3, errorMsgRef)) {
-    }
-    else if (fields[2] === fields[3]) {
-        errorMsgRef.current.innerHTML = "Passwords match";
-        errorMsgRef.current.className = "errorMsg good";
-        return true;
-    }
-    else {
-        errorMsgRef.current.innerHTML = "Passwords don't match";
-        errorMsgRef.current.className = "errorMsg bad";
-    }
-    return false;
-}
-
-export function attemptCreateAccount(fields, emptyMsgs, errorMsgRef) {
-    // See previous comment
-    if (checkPasswordsMatch(fields, emptyMsgs, errorMsgRef)) {
-        localStorage.setItem(fields[1] + "_password", fields[2]);
-        localStorage.setItem(fields[0] + "_email_taken", "t");
-        document.location.href = "/";
-    }
-}
-
-export function checkValidUsername(fields, emptyMsgs, errorMsgRef) {
-    if (!notEmpty(fields, emptyMsgs, 0, errorMsgRef)) {
-    }
-    else if (localStorage.getItem(fields[0] + "_password")) {
-        errorMsgRef.current.innerHTML = "Username valid";
-        errorMsgRef.current.className = "errorMsg good";
-        return true;
-    }
-    else {
-        errorMsgRef.current.innerHTML = "Username invalid";
-        errorMsgRef.current.className = "errorMsg bad";
-    }
-    return false;
-}
-
-export function checkLoginPassword(fields, emptyMsgs, errorMsgRef) {
-    if (!checkValidUsername(fields, emptyMsgs, errorMsgRef) ||
-        !notEmpty(fields, emptyMsgs, 1, errorMsgRef)) {
-            return false;
-    }
-    clearError(errorMsgRef);
-
-    return true;
-}
-
-export function submitLoginInfo(fields, emptyMsgs, errorMsgRef) {
-    var real_password;
-    if (checkLoginPassword(fields, emptyMsgs, errorMsgRef)) {
-        real_password = localStorage.getItem(fields[0] + "_password");
-
-        if (fields[1] === real_password) {
-            localStorage.setItem("username", fields[0]);
-            document.location.href = "/game";
-        }
-        else {
-            errorMsgRef.current.innerHTML = "Invalid password";
-            errorMsgRef.current.className = "errorMsg bad";
-        }
-    }
-}
-
-export function clearError(errorMsgRef) {
-    errorMsgRef.current.innerHTML = "";
-    errorMsgRef.current.className = "errorMsg good";
-}
-*/
--- src/home/home_aux.jsx
+++ src/home/home_aux.jsx
@@ -11,7 +11,7 @@ function notEmpty(fields, emptyMsgs, index, errorMsgRef) {
     return true;
 }
 
-export async function checkUniqueEmail(fields, emptyMsgs, errorMsgRef) {
+export function checkUniqueEmail(fields, emptyMsgs, errorMsgRef) {
     function setMessage(goodEmail) {
         if (goodEmail) {
             errorMsgRef.current.innerHTML = "";
@@ -59,10 +59,13 @@ export async function checkUniqueEmail(fields, emptyMsgs, errorMsgRef) {
                 resolve(false);
             }
         }
+        else {
+            resolve(false);
+        }
     });
 }
 
-export async function checkUniqueUsername(fields, emptyMsgs, errorMsgRef) {
+export function checkUniqueUsername(fields, emptyMsgs, errorMsgRef) {
     /* The reason why I do all the previous check along with the current check, even if 
     the previous checks have already been done, is because I want the error messages 
     from the first fields that you fill out to have priority. I'm a little nitpicky 
@@ -81,7 +84,7 @@ export async function checkUniqueUsername(fields, emptyMsgs, errorMsgRef) {
     return new Promise((resolve, reject) => {
         checkUniqueEmail(fields, emptyMsgs, errorMsgRef).then(
             (result) => {
-                if (result) {
+                if (result === true) {
                     if (notEmpty(fields, emptyMsgs, 1, errorMsgRef)) {
                         var cachedResult = usernamesCache[fields[1]];
                         if (cachedResult === undefined) {
@@ -116,6 +119,12 @@ export async function checkUniqueUsername(fields, emptyMsgs, errorMsgRef) {
                             resolve(false);
                         }
                     }
+                    else {
+                        resolve(false);
+                    }
+                }
+                else {
+                    resolve(false);
                 }
             }
         );
@@ -127,12 +136,17 @@ export function checkRegPassword(fields, emptyMsgs, errorMsgRef) {
     return new Promise((resolve, reject) => {
         checkUniqueUsername(fields, emptyMsgs, errorMsgRef).then(
             (result) => {
-                console.log("checking username after blur password: " + result);
-                if (result) {
+                if (result === true) {
                     if (notEmpty(fields, emptyMsgs, 2, errorMsgRef)) {
                         clearError(errorMsgRef);
                         resolve(true);
                     }
+                    else {
+                        resolve(false);
+                    }
+                }
+                else {
+                    resolve(false);
                 }
             }
         );
@@ -141,28 +155,50 @@ export function checkRegPassword(fields, emptyMsgs, errorMsgRef) {
 
 export function checkPasswordsMatch(fields, emptyMsgs, errorMsgRef) {
     // See previous comment
-    if (!checkRegPassword(fields, emptyMsgs, errorMsgRef) ||
-        !notEmpty(fields, emptyMsgs, 3, errorMsgRef)) {
-    }
-    else if (fields[2] === fields[3]) {
-        errorMsgRef.current.innerHTML = "Passwords match";
-        errorMsgRef.current.className = "errorMsg good";
-        return true;
-    }
-    else {
-        errorMsgRef.current.innerHTML = "Passwords don't match";
-        errorMsgRef.current.className = "errorMsg bad";
-    }
-    return false;
+    return new Promise((resolve, reject) => {
+        checkRegPassword(fields, emptyMsgs, errorMsgRef).then(
+            (result) => {
+                if (result === true) {
+                    if (notEmpty(fields, emptyMsgs, 2, errorMsgRef)) {
+                        if (fields[2] === fields[3]) {
+                            errorMsgRef.current.innerHTML = "Passwords match";
+                            errorMsgRef.current.className = "errorMsg good";
+                            resolve(true);
+                        }
+                        else {
+                            errorMsgRef.current.innerHTML = "Passwords don't match";
+                            errorMsgRef.current.className = "errorMsg bad";
+                            resolve(false);
+                        }
+                    }
+                    else {
+                        resolve(false);
+                    }
+                }
+                else {
+                    resolve(false);
+                }
+            }
+        );
+    });
 }
 
 export function attemptCreateAccount(fields, emptyMsgs, errorMsgRef) {
     // See previous comment
-    if (checkPasswordsMatch(fields, emptyMsgs, errorMsgRef)) {
-        localStorage.setItem(fields[1] + "_password", fields[2]);
-        localStorage.setItem(fields[0] + "_email_taken", "t");
-        document.location.href = "/";
-    }
+    checkPasswordsMatch(fields, emptyMsgs, errorMsgRef).then((result) => {
+        if (result === true) {
+            fetch("/api/account", {
+                method: "post",
+                body: JSON.stringify({email: fields[0], username: fields[1], password: fields[2]}),
+                headers: { "Content-type": "application/json; charset=UTF-8", }
+            }).then((result) => {
+                console.log(result.body);
+                if (result.status === 201) {
+                    document.location.href = "/";
+                }
+            });
+        }
+    });
 }
 
 export function checkValidUsername(fields, emptyMsgs, errorMsgRef) {
